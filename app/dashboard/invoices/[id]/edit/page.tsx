2fd707aa8a133c48f16dfc60ce45d24a1488d712
md-alif-jasim <mdalifjasim@gmail.com>
2024-12-03 23:17:19 -0500

    ch12 part2

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,12 +2,14 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-    const params = await props.params;
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params; // Access `id` directly from `params`
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
   return (
     <main>
       <Breadcrumbs
